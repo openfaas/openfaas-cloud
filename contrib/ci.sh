0a72466838248f0718b82442e414fc2335ccb7ca
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-05-15 21:03:24 +0100

    Install CLI via release page
    The curl script was running out of API requests from GitHub
    due to using a shared CI machine / exit IP
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -10,7 +10,8 @@ CLI="faas-cli"
 if ! [ -x "$(command -v faas-cli)" ]; then
     HERE=`pwd`
     cd /tmp/
-    curl -sL cli.openfaas.com | sh
+    curl -sL https://github.com/openfaas/faas-cli/releases/download/0.6.9/faas-cli > faas-cli
+    chmod +x ./faas-cli
     CLI="/tmp/faas-cli"
 
     echo "Returning to $HERE"
