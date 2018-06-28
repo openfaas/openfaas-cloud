8ac87dbf5148e825c18cfe04310ca589f2a318be
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-06-28 10:48:22 +0300

    Revendor github-push with dep
    This allows us to prune out many files which are not required, but
    which are present in the sdk package.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- github-push/vendor/github.com/alexellis/hmac/pkg.go
+++ github-push/vendor/github.com/alexellis/hmac/pkg.go
@@ -16,12 +16,30 @@ func CheckMAC(message, messageMAC, key []byte) bool {
 	return hmac.Equal(messageMAC, expectedMAC)
 }
 
-// Validate validate a digest from Github via xHubSignature
-func Validate(bytesIn []byte, xHubSignature string, secretKey string) error {
+// Sign a message with the key and return bytes.
+// Note: for human readable output see encoding/hex and
+// encode string functions.
+func Sign(message, key []byte) []byte {
+	mac := hmac.New(sha1.New, key)
+	mac.Write(message)
+	signed := mac.Sum(nil)
+	return signed
+}
+
+// Validate validate an encodedHash taken
+// from GitHub via X-Hub-Signature HTTP Header.
+// Note: if using another source, just add a 5 letter prefix such as "sha1="
+func Validate(bytesIn []byte, encodedHash string, secretKey string) error {
 	var validated error
 
-	if len(xHubSignature) > 5 {
-		messageMAC := xHubSignature[5:] // first few chars are: sha1=
+	if len(encodedHash) > 5 {
+
+		hashingMethod := encodedHash[:5]
+		if hashingMethod != "sha1=" {
+			return fmt.Errorf("unexpected hashing method: %s", hashingMethod)
+		}
+
+		messageMAC := encodedHash[5:] // first few chars are: sha1=
 		messageMACBuf, _ := hex.DecodeString(messageMAC)
 
 		res := CheckMAC(bytesIn, []byte(messageMACBuf), []byte(secretKey))
@@ -29,7 +47,7 @@ func Validate(bytesIn []byte, xHubSignature string, secretKey string) error {
 			validated = fmt.Errorf("invalid message digest or secret")
 		}
 	} else {
-		return fmt.Errorf("invalid xHubSignature, should have at least 5 characters")
+		return fmt.Errorf("invalid encodedHash, should have at least 5 characters")
 	}
 
 	return validated
