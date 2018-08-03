0dc919507689e058c082c394d93ab0331fe28de1
Alex Ellis <alexellis2@gmail.com>
2018-08-03 19:37:51 +0100

    Move builder URL
    Builder URL moved to gateway_config.yml
    
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ of-builder/deploy_swarm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+docker service rm registry
+docker service create --network func_functions \
+  --name registry \
+  --detach=true -p 5000:5000 registry:latest
+
+docker rm -f of-buildkit
+docker run -d --net func_functions -d --privileged \
+--restart always \
+--name of-buildkit  alexellis2/buildkit:2018-04-17 --addr tcp://0.0.0.0:1234
+
+export OF_BUILDER_TAG=0.4.2
+
+docker service rm of-builder
+docker service create --detach=true --network func_functions --name of-builder openfaas/of-builder:$OF_BUILDER_TAG
+
+
