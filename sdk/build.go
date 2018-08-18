66d2c5373e6316fb05cf777c2312f436a33bf551
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-08-18 11:55:52 +0100

    Add BuildResult / PipelineLog
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- sdk/build.go
+++ sdk/build.go
@@ -1,5 +1,9 @@
 package sdk
 
-type Build struct {
-	Line []string
+// BuildResult represents a successful Docker build and
+// push operation to a remote registry
+type BuildResult struct {
+	Log       []string `json:"log"`
+	ImageName string   `json:"imageName"`
+	Status    string   `json:"status"`
 }
