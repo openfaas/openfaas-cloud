21fb414c44a0c49ba7248d342cf48a633b09aa4c
Bart Smykla (VMware) <bartek@smykla.com>
2018-09-18 09:14:22 +0100

    Added const FunctionLabelPrefix to sdk
    FunctionLabelPrefix = "com.openfaas.cloud."
    
    Signed-off-by: Bart Smykla (VMware) <bartek@smykla.com>

--- sdk/constants.go
+++ sdk/constants.go
@@ -3,4 +3,6 @@ package sdk
 const (
 	//CloudSignatureHeader header name to pass signed payload secret
 	CloudSignatureHeader = "X-Cloud-Signature"
+	// FunctionLabelPrefix is a prefix for openfaas labels inside functions
+	FunctionLabelPrefix = "com.openfaas.cloud."
 )
