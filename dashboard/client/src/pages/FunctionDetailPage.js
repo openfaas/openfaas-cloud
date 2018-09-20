a56fa600af3f8f1809a7054039de53eb4c764264
Jaigouk Kim <ping@jaigouk.kim>
2018-09-20 23:29:00 +0200

    add badge button
    - fixes #172
    - added react-bootstrap
    - added a button to open a modal that has pre-filled text input
    - show the preview of badge in the modal
    
    Signed-off-by: Jaigouk Kim <ping@jaigouk.kim>

--- dashboard/client/src/pages/FunctionDetailPage.js
+++ dashboard/client/src/pages/FunctionDetailPage.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { Link } from 'react-router-dom';
 import { functionsApi } from '../api/functionsApi';
 import { FunctionDetailSummary } from '../components/FunctionDetailSummary';
+import { Modal, Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 export class FunctionDetailPage extends Component {
   constructor(props) {
     super(props);
     const { repoPath } = queryString.parse(props.location.search);
     const { user, functionName } = props.match.params;
+    this.handleShowBadgeModal = this.handleShowBadgeModal.bind(this);
+    this.handleCloseBadgeModal = this.handleCloseBadgeModal.bind(this);
     this.state = {
       isLoading: true,
       fn: null,
       user,
       repoPath,
       functionName,
-    };
+      showBadgeModal: false,
+    };    
   }
   componentDidMount() {
     const { user, repoPath, functionName } = this.state;
@@ -25,10 +29,20 @@ export class FunctionDetailPage extends Component {
       this.setState({ isLoading: false, fn: res });
     });
   }
+  handleCloseBadgeModal() {
+    this.setState({ showBadgeModal: false });
+  }
+
+  handleShowBadgeModal() {
+    this.setState({ showBadgeModal: true });
+  }
   render() {
     const { isLoading, fn } = this.state;
-
+    const badgeImage = "https://img.shields.io/badge/openfaas-cloud-blue.svg"
+    const badgeLink = "https://www.openfaas.com"
+    const badgeMd = `[![OpenFaaS](${badgeImage})](${badgeLink})`
     let panelBody;
+    
     if (isLoading) {
       panelBody = (
         <div className="panel-body">
@@ -40,15 +54,37 @@ export class FunctionDetailPage extends Component {
     } else {
       panelBody = (
         <div className="panel-body">
-          <FunctionDetailSummary fn={fn} />
+          <FunctionDetailSummary fn={fn} handleShowBadgeModal={this.handleShowBadgeModal}/>
         </div>
       );
     }
-
     return (
       <div className="panel panel-success">
         <div className="panel-heading">Function Overview</div>
         {panelBody}
+          <Modal show={this.state.showBadgeModal} onHide={this.handleCloseBadgeModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>Get Badge</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>
+                <Link to={badgeLink}>
+                  <img src={badgeImage} alt="OpenFaaS" />
+                </Link>
+              </p>
+              <form>
+                <FormGroup
+                  controlId="formGetBadge"
+                >
+                  <FormControl
+                    type="text"
+                    value={badgeMd}
+                  />
+                  <FormControl.Feedback />
+                </FormGroup>
+              </form>
+            </Modal.Body>
+          </Modal>
       </div>
     );
   }
