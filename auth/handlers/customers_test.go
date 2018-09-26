98ec92e0ca04179c7184e2853a247d9819736d8b
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-09-26 08:18:39 +0100

    Implement feedback from fntlz
    - Lorenzo suggested modifying use of Lock/Unlock with defer, this
    change means the code will still be run if there is an unexpected
    error in the block that takes the lock.
    
    - Added asserts for the GitHub customers-file check.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- auth/handlers/customers_test.go
+++ auth/handlers/customers_test.go
@@ -1,14 +1,43 @@
 package handlers
 
 import (
-	"log"
 	"testing"
 )
 
-func TestGet(t *testing.T) {
+func TestGet_InvalidCustomerLiveGitHubFile(t *testing.T) {
 	c := NewCustomers()
-	log.Println(c.Get("alexellis"))
-	log.Println(c.Get("alexellisuk"))
-	log.Println(c.Get("AlexEllisUK"))
-	log.Println(c.Get("rgee0"))
+
+	valid := []string{"not-alexellis"}
+
+	for _, user := range valid {
+		val, err := c.Get(user)
+		if err != nil {
+			t.Errorf("error fetching users: %s", err.Error())
+			t.Fail()
+		}
+
+		if val != false {
+			t.Errorf("user %s should not be a customer, but was", user)
+			t.Fail()
+		}
+	}
+}
+
+func TestGet_ExistingCustomerLiveGitHubFile(t *testing.T) {
+	c := NewCustomers()
+
+	valid := []string{"alexellis", "rgee0", "LucasRoesler"}
+
+	for _, user := range valid {
+		val, err := c.Get(user)
+		if err != nil {
+			t.Errorf("error fetching users: %s", err.Error())
+			t.Fail()
+		}
+
+		if val != true {
+			t.Errorf("user %s should be a customer, but wasn't", user)
+			t.Fail()
+		}
+	}
 }
