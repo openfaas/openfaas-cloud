658ff55b56a9ad740ab023cd6cacb0c031ba76d8
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-09-27 15:20:22 +0100

    Make of-builder use internal trust secret
    - this prevents of-builder from being called by other services
    which may be able to reach the TCP socket within the network
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- of-builder/vendor/github.com/sirupsen/logrus/text_formatter.go
+++ of-builder/vendor/github.com/sirupsen/logrus/text_formatter.go
@@ -26,7 +26,6 @@ func init() {
 	baseTimestamp = time.Now()
 }
 
-// TextFormatter formats logs into text
 type TextFormatter struct {
 	// Set to true to bypass checking for a TTY before outputting colors.
 	ForceColors bool
@@ -53,6 +52,10 @@ type TextFormatter struct {
 	// QuoteEmptyFields will wrap empty fields in quotes if true
 	QuoteEmptyFields bool
 
+	// QuoteCharacter can be set to the override the default quoting character "
+	// with something else. For example: ', or `.
+	QuoteCharacter string
+
 	// Whether the logger's out is to a terminal
 	isTerminal bool
 
@@ -60,12 +63,14 @@ type TextFormatter struct {
 }
 
 func (f *TextFormatter) init(entry *Entry) {
+	if len(f.QuoteCharacter) == 0 {
+		f.QuoteCharacter = "\""
+	}
 	if entry.Logger != nil {
-		f.isTerminal = checkIfTerminal(entry.Logger.Out)
+		f.isTerminal = IsTerminal(entry.Logger.Out)
 	}
 }
 
-// Format renders a single log entry
 func (f *TextFormatter) Format(entry *Entry) ([]byte, error) {
 	var b *bytes.Buffer
 	keys := make([]string, 0, len(entry.Data))
@@ -90,7 +95,7 @@ func (f *TextFormatter) Format(entry *Entry) ([]byte, error) {
 
 	timestampFormat := f.TimestampFormat
 	if timestampFormat == "" {
-		timestampFormat = defaultTimestampFormat
+		timestampFormat = DefaultTimestampFormat
 	}
 	if isColored {
 		f.printColored(b, entry, keys, timestampFormat)
@@ -156,12 +161,11 @@ func (f *TextFormatter) needsQuoting(text string) bool {
 }
 
 func (f *TextFormatter) appendKeyValue(b *bytes.Buffer, key string, value interface{}) {
-	if b.Len() > 0 {
-		b.WriteByte(' ')
-	}
+
 	b.WriteString(key)
 	b.WriteByte('=')
 	f.appendValue(b, value)
+	b.WriteByte(' ')
 }
 
 func (f *TextFormatter) appendValue(b *bytes.Buffer, value interface{}) {
@@ -173,6 +177,13 @@ func (f *TextFormatter) appendValue(b *bytes.Buffer, value interface{}) {
 	if !f.needsQuoting(stringVal) {
 		b.WriteString(stringVal)
 	} else {
-		b.WriteString(fmt.Sprintf("%q", stringVal))
+		b.WriteString(f.quoteString(stringVal))
 	}
+}
+
+func (f *TextFormatter) quoteString(v string) string {
+	escapedQuote := fmt.Sprintf("\\%s", f.QuoteCharacter)
+	escapedValue := strings.Replace(v, f.QuoteCharacter, escapedQuote, -1)
+
+	return fmt.Sprintf("%s%v%s", f.QuoteCharacter, escapedValue, f.QuoteCharacter)
 }
