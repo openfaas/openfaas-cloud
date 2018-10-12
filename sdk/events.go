74cbcdc7d6a3a0e21cbc092a7daff8fa0aebe367
Martin Dekov (VMware) <mdekov@vmware.com>
2018-10-12 08:37:55 -0700

    Add struct to sdk that handles GitLab payload
    Adding struct that handles GitLab payload cause currently
    PR #109 that handles push events from GitLab use copied
    vendor folder which is not the right way to vendor code
    the PR above need proper vendoring
    
    Signed-off-by: Martin Dekov (VMware) <mdekov@vmware.com>

--- sdk/events.go
+++ sdk/events.go
@@ -41,6 +41,28 @@ type Event struct {
 	Private        bool              `json:"private"`
 }
 
+// PushEvent as received from GitLab
+
+type GitLabPushEvent struct {
+	Ref              string           `json:"ref"`
+	UserUsername     string           `json:"user_username"`
+	UserEmail        string           `json:"user_email"`
+	GitLabProject    GitLabProject    `json:"project"`
+	GitLabRepository GitLabRepository `json:"repository"`
+	AfterCommitID    string           `json:"after"`
+}
+
+type GitLabProject struct {
+	ID                int    `json:"id"`
+	Namespace         string `json:"namespace"`
+	Name              string `json:"name"`
+	PathWithNamespace string `json:"path_with_namespace"` //would be repo full name
+}
+
+type GitLabRepository struct {
+	CloneURL string `json:"git_http_url"`
+}
+
 type Customer struct {
 	Sender Sender `json:"sender"`
 }
