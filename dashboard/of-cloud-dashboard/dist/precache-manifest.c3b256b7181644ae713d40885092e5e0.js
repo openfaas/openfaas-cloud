8a036e4dda154ad2b3559c77e9624e77d241a294
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-09-27 11:18:13 +0100

    Upgrade React / Node.js tooling
    Upgrades to quash warnings from GitHub.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ dashboard/of-cloud-dashboard/dist/precache-manifest.c3b256b7181644ae713d40885092e5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d468ec5e7b8f8a35b3605631e893199",
+    "url": "dist/index.html"
+  },
+  {
+    "revision": "beb41910db6ce60d34f3",
+    "url": "dist/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bc090747d3fc4982be76",
+    "url": "dist/static/css/main.2e5a1c5d.chunk.css"
+  },
+  {
+    "revision": "beb41910db6ce60d34f3",
+    "url": "dist/static/js/2.0aff00b1.chunk.js"
+  },
+  {
+    "revision": "bc090747d3fc4982be76",
+    "url": "dist/static/js/main.a152d954.chunk.js"
+  },
+  {
+    "revision": "180fb92545f43668e653",
+    "url": "dist/static/js/runtime-main.1ef952e7.js"
+  }
+]);
