1ff11b8f2dbd494af1b223ab0c7917b7b4e6e7a9
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-03-05 15:05:34 +0000

    Add test to unmarshal organization
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- edge-auth/provider/types_test.go
+++ edge-auth/provider/types_test.go
@@ -1,6 +1,10 @@
 package provider
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
 
 func TestSupportedProviders_IsSupported_whenProviderIsSupported(t *testing.T) {
 	if !IsSupported("github") {
@@ -19,5 +23,27 @@ func TestSupportedProviders_IsSupported_whenProviderIsSupported(t *testing.T) {
 func TestSupportedProviders_IsSupported_whenProviderIsNotSupported(t *testing.T) {
 	if IsSupported("foobar.com") {
 		t.Errorf("Want: \"%v\". Got: \"%v\"", false, true)
+	}
+}
+
+func TestUnmarshalOrg(t *testing.T) {
+	wantID := 49474643
+	wantLogin := "teamserverless"
+	orgVal := []byte(fmt.Sprintf(`{"login": "%s","id": %d}`, wantLogin, wantID))
+
+	org := Organization{}
+	err := json.Unmarshal(orgVal, &org)
+
+	if err != nil {
+		t.Errorf("received error, wanted none: %s", err.Error())
+		t.Fail()
+	}
+	if org.ID != wantID {
+		t.Errorf("ID want: %d, got: %d", wantID, org.ID)
+		t.Fail()
+	}
+	if org.Login != wantLogin {
+		t.Errorf("Login want: %s, got: %s", wantLogin, org.Login)
+		t.Fail()
 	}
 }
