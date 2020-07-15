cf722c771a43b3ac215c1edb4c496b135369fc1e
Alistair Hey <alistair@heyal.co.uk>
2020-07-15 20:38:41 +0100

    Upgrade derek dependency to 0.10.2
    This version of derek includes an update to remove a soon to be
    depricated API endpoint on github.
    Reference: https://developer.github.com/changes/2020-04-15-replacing-create-installation-access-token-endpoint/
    
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- github-status/vendor/github.com/alexellis/derek/auth/jwt_auth.go
+++ github-status/vendor/github.com/alexellis/derek/auth/jwt_auth.go
@@ -13,13 +13,59 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-// GetSignedJwtToken get a tokens signed with private key
-func GetSignedJwtToken(appID string, privateKeyPath string) (string, error) {
+// JWTAuth token issued by Github in response to signed JWT Token
+type JWTAuth struct {
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
 
-	keyBytes, err := ioutil.ReadFile(privateKeyPath)
+// MakeAccessTokenForInstallation makes an access token for an installation / private key
+func MakeAccessTokenForInstallation(appID string, installation int, privateKey string) (string, error) {
+	signed, err := GetSignedJwtToken(appID, privateKey)
+
 	if err != nil {
-		return "", fmt.Errorf("unable to read private key path: %s, error: %s", privateKeyPath, err)
+		msg := fmt.Sprintf("can't run GetSignedJwtToken for app_id: %s and installation_id: %d, error: %v", appID, installation, err)
+
+		fmt.Printf("Error %s\n", msg)
+		return "", err
 	}
+
+	req, err := http.NewRequest(http.MethodPost,
+		fmt.Sprintf("https://api.github.com/app/installations/%d/access_tokens", installation), nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", signed))
+	req.Header.Add("Accept", "application/vnd.github.machine-man-preview+json")
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		msg := fmt.Sprintf("can't get access_token for app_id: %s and installation_id: %d error: %v", appID, installation, err)
+		fmt.Printf("Error: %s\n", msg)
+		return "", fmt.Errorf("%s", msg)
+	}
+
+	defer res.Body.Close()
+
+	bytesOut, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return "", readErr
+	}
+
+	jwtAuth := JWTAuth{}
+	jsonErr := json.Unmarshal(bytesOut, &jwtAuth)
+	if jsonErr != nil {
+		return "", jsonErr
+	}
+	return jwtAuth.Token, nil
+}
+
+// GetSignedJwtToken get a tokens signed with private key
+func GetSignedJwtToken(appID string, privateKey string) (string, error) {
+
+	keyBytes := []byte(privateKey)
 
 	key, keyErr := jwt.ParseRSAPrivateKeyFromPEM(keyBytes)
 	if keyErr != nil {
@@ -41,42 +87,4 @@ func GetSignedJwtToken(appID string, privateKeyPath string) (string, error) {
 	}
 
 	return string(signedVal), nil
-}
-
-// JwtAuth token issued by Github in response to signed JWT Token
-type JwtAuth struct {
-	Token     string    `json:"token"`
-	ExpiresAt time.Time `json:"expires_at"`
-}
-
-// MakeAccessTokenForInstallation makes an access token for an installation / private key
-func MakeAccessTokenForInstallation(appID string, installation int, privateKeyPath string) (string, error) {
-	signed, err := GetSignedJwtToken(appID, privateKeyPath)
-
-	if err == nil {
-		c := http.Client{}
-		req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.github.com/installations/%d/access_tokens", installation), nil)
-
-		req.Header.Add("Authorization", "Bearer "+signed)
-		req.Header.Add("Accept", "application/vnd.github.machine-man-preview+json")
-
-		res, err := c.Do(req)
-
-		if err == nil {
-			defer res.Body.Close()
-			bytesOut, readErr := ioutil.ReadAll(res.Body)
-			if readErr != nil {
-				return "", readErr
-			}
-			jwtAuth := JwtAuth{}
-			jsonErr := json.Unmarshal(bytesOut, &jwtAuth)
-			if jsonErr != nil {
-				return "", jsonErr
-			}
-
-			return string(jwtAuth.Token), nil
-		}
-	}
-
-	return "", err
 }
